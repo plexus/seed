dac0429b350b9fd1da074a92ce5bbf054aefc1ab
Arne Brasseur <arne@arnebrasseur.net>
2014-12-02 11:10:55 +0100

    whitespace

--- spec/seed/core_spec.clj
+++ spec/seed/core_spec.clj
@@ -7,16 +7,14 @@
 
   (describe "seed.core/set-ns"
 
-            (it "should set the namespace name"
-                (should= "(ns hello.world\n  (:require [foo.bar :as bar]))"
-                         (to-string (set-ns zipper 'hello.world))
-                         )))
+    (it "should set the namespace name"
+        (should= "(ns hello.world\n  (:require [foo.bar :as bar]))"
+                 (to-string (set-ns zipper 'hello.world)))))
 
-    (describe "seed.core/add-require"
+  (describe "seed.core/add-require"
 
-            (it "should add a requirement to the list"
-                (should= "(ns foo\n  (:require [foo.bar :as bar] \n            [baz :refer all]))"
-                         (to-string (add-require zipper '[baz :refer all]))
-                         ))))
+    (it "should add a requirement to the list"
+      (should= "(ns foo\n  (:require [foo.bar :as bar] \n            [baz :refer all]))"
+               (to-string (add-require zipper '[baz :refer all]))))))
 
 (run-specs)
